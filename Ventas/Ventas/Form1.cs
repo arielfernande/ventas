bf6f5f99e912baf982737b8d70d422a9e39aae4b
Ariel Martinez <ariel_isaimartinez@hotmail.com>
2019-02-28 11:29:54 -0600

    Agregando Clase Producto y Clase Categoria

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,42 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("piensa, sueña, atrevete");
+            //Categorias
+            var categoria1 = new Categoria();
+            categoria1.Id = 1;
+            categoria1.Nombre = "Celulares";
+
+            var categoria2 = new Categoria();
+            categoria2.Id = 1;
+            categoria2.Nombre = "Accesorios";
+
+            //Productos
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Celular Iphone";
+            producto1.Precio = 10000;
+            producto1.Categoria = categoria1;
+
+            Producto producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Celular Samsung";
+            producto2.Precio = 8000;
+            producto2.Categoria = categoria1;
+
+            Producto producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Audifonos";
+            producto3.Precio = 300;
+            producto3.Categoria = categoria2;
+
+            var listadeproductos = new List<Producto>();
+            listadeproductos.Add(producto1);
+            listadeproductos.Add(producto2);
+            listadeproductos.Add(producto3);
+
+            foreach (var p in listadeproductos)
+
+                MessageBox.Show(p.Descripcion + " " + p.Categoria.Nombre);
         }
     }
 }
